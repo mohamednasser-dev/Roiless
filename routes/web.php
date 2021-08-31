8bfa730821fb7caa6d2de92d80388649d8d0228f
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-08-31 13:13:32 +0200

    make changes

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Auth::routes();
 
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/login_user', 'Admin\LoginController@login')->name('login_user');
 
 Route::group(['middleware' => ['auth', 'admin']], function () {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
     //users  routes
     Route::resource('users', 'Admin\usersController');
     Route::get('users/{id}/delete', 'Admin\usersController@destroy')->name('users.delete');
