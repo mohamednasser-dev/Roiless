4872bf4810c421de27cb94100a9c71485f49bc34
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-10-05 21:44:12 +0200

    fix cost

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,7 @@ Route::group(['namespace' => 'API', 'middleware' => ['api']], function () {
 //fund detailes
         Route::get("/fund/detailes/{id}", "FundController@getfunddetailes");
         Route::post("/funds/addfund", "FundController@addfund");
+        Route::post("/users/consolutions/store", "UsersController@consolutions_store");
 
 //user update
         Route::post("/update_profile", "UsersController@updateProfile");
