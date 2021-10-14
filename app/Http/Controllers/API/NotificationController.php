9011a55735ad025ca9038feebe7e9fe2381274a1
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-10-14 17:11:07 +0200

    jj

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Notification;
+use App\Models\User_Notification;
+use Illuminate\Support\Facades\Auth;
 class NotificationController extends Controller
 {
     public function getall(Request $request)
     {
-        $notificaations = Notification::select(['id','title_ar','title_en', 'body_ar', 'body_en','image'])->get();
-        return msgdata($request, success(), 'get services sucess',$notificaations);
+        //  $notificaations = Notification::select(['id','title_ar','title_en', 'body_ar', 'body_en','image'])::with()->get();
+        //    $notificaations=Notification::with('user',function($q){
+               
+        //    })->get();
+       
+            $notificaations=User_Notification::where('user_id',Auth::user()->id)->with('notifications')->get();
+            return msgdata($request, success(), 'get services sucess',$notificaations);
+      
+       
     }
 }
