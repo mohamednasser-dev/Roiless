383d258b83cb168afe5f7600aea79ba2ab7d8552
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2021-10-17 14:59:28 +0200

    disaple_bank

---
+++ database/migrations/2021_10_14_141943_update_notification_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('user_notifications', function (Blueprint $table) {
+            $table->integer('seen')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
