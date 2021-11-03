e3da78706e65e16ec9754d7c0e98bde305e3ab15
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2021-11-03 13:39:25 +0200

    select all users update

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -14,7 +14,7 @@ class NotificationController extends Controller
     public function getall(Request $request)
     {
         //  $notificaations = Notification::select(['id','title_ar','title_en', 'body_ar', 'body_en','image'])::with()->get();
-        //    $notificaations=Notification::with('user',function($q){   
+        //    $notificaations=Notification::with('user',function($q){
         //    })->get();
         $id=Auth::user()->id;
         $notificaations=User_Notification::where('user_id',$id)->with('notifications')->orderBy('created_at','Desc')->get();
