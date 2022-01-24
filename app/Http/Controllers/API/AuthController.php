43143fc58e348e76ec982eab6f0aea27e308a81c
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-01-24 22:25:43 +0200

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,6 +22,7 @@ use Validator;
 use App\Models\User;
 use PDF;
 use DB;
+use Ghanem\LaravelSmsmisr\Facades\Smsmisr;
 
 class AuthController extends Controller
 {
@@ -131,25 +132,9 @@ class AuthController extends Controller
         if ($validator->fails()) {
             return response()->json(['status' => 401, 'msg' => $validator->messages()->first()]);
         } else {
+            $send = Smsmisr::send("hello world", "201095055833");
+            dd($send);
             //Request is valid, create new user
-$url = 'https://smsmisr.com/api/webapi/?';
-$curl = curl_init();
-$fields = array(
-    'username' => 'K0WPCrHX',
-    'password' => '8kZKSusuDE',
-    'language' => '2',
-    'sender'   => 'roilleass',
-    'mobile'   => '201095055833',
-    'message'  => 'XXX'
-);
-$fields_string = http_build_query($fields);
-dd($fields_string);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, TRUE);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
-$data = curl_exec($curl);
-curl_close($curl);          
-            return $data;
             $data['otp_code'] = '123456';
             $data['fcm_token']=$request->fcm_token;
             $user = User::create($data);
