ef9e76558e22e9e40f00ffa52ebe9969aa454746
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-04-18 11:32:08 +0200

    return middle Middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -17,10 +17,10 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
 
-        if (!$request->secure() && App::environment() === 'production') {
-            $request->setTrustedProxies( [ $request->getClientIp() ] );
-            return redirect()->secure($request->getRequestUri());
-        }
+//        if (!$request->secure() && App::environment() === 'production') {
+//            $request->setTrustedProxies( [ $request->getClientIp() ] );
+//            return redirect()->secure($request->getRequestUri());
+//        }
 
         return $next($request);
     }
