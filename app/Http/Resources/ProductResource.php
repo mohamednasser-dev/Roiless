73f25f79f36837ab3c92eb992cade0ad527dbcce
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-05-15 12:41:48 +0200

    fix seeler id

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -24,6 +24,7 @@ class ProductResource extends JsonResource
             'quantity' => $this->quantity,
             'image_path' => $this->image_path,
             'type' => $this->type,
+            'seller_id' => $this->seller_id,
             'section' => $this->Section,
             'sub_section' => $this->SubSection,
 //            'seller_product_count' => $this->SellerInfo->Products->count(),
