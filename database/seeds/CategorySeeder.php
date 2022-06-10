e84d63f55e801d8ca9ae5e8762a562d050120663
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-06-10 02:46:15 +0200

    m

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use App\Models\City;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -17,5 +18,35 @@ class CategorySeeder extends Seeder
             'title_en'  => 'Estate',
             'type'  => 'cat',
         ]);
+
+        $benefit_data = [
+            [
+                'id' => 1,
+                'name_ar' => 'مصر',
+                'name_en' => 'Egypt',
+                'country_code' => '+20',
+            ],
+            [
+                'id' => 2,
+                'name_ar' => 'السعودية',
+                'name_en' => 'Saudi Arabia',
+                'country_code' => '+966',
+            ],
+            [
+                'id' => 3,
+                'name_ar' => 'الكويت',
+                'name_en' => 'Kuwait',
+                'country_code' => '+965',
+            ],
+            [
+                'id' => 4,
+                'name_ar' => 'قطر',
+                'name_en' => 'Qatar',
+                'country_code' => '+974',
+            ],
+        ];
+        foreach ($benefit_data as $get) {
+            City::updateOrCreate($get);
+        }
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CitySeeder::class);
-        $this->call(UserSeeder::class);
+         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(SettingSeeder::class);
         $this->call(FundinputesSeeder::class);
